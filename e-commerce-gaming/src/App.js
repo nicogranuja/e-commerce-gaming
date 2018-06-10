67b0a4b4868a3ebde5cd1dc28343de3db69ed4d4
Justin Morales <Justin@justins-mbp.attlocal.net>
2018-06-10 18:37:14 -0500

    Header and Initial Buttons for Consoles

--- e-commerce-gaming/src/App.js
+++ e-commerce-gaming/src/App.js
@@ -1,19 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Home from './Components/HomePage';
+import MuiThemeProvider from  '@material-ui/core/styles/MuiThemeProvider'
 import './App.css';
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <MuiThemeProvider>
+          <AppBar position="static" color="default">
+              <Toolbar>
+                  <Typography variant="title" color="inherit">
+                      Games E - Commerce
+                  </Typography>
+              </Toolbar>
+              <Tabs
+
+                  indicatorColor="primary"
+                  textColor="primary"
+                  fullWidth
+              >
+                  <Tab label="Xbox One" />
+                  <Tab label="PS 4" />
+                  <Tab label="Nintendo" />
+                  <Tab label="Computer" />
+                  <Tab label="Game Boy" />
+              </Tabs>
+          </AppBar>
+      </MuiThemeProvider>
     );
   }
 }
