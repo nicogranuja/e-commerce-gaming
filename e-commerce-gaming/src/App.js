0b22f493ce210370e737c995aa66e0588e40c888
Juan Diaz Sada <jdsada30@gmail.com>
2018-06-12 14:21:01 -0500

    added list of games
    hardcoded some games and added list display

--- e-commerce-gaming/src/App.js
+++ e-commerce-gaming/src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import Home from './Components/HomePage';
+import React, { Component, Fragment } from 'react';
+//import Home from './Components/HomePage';
 import MuiThemeProvider from  '@material-ui/core/styles/MuiThemeProvider'
 import './App.css';
 import AppBar from '@material-ui/core/AppBar'
@@ -7,10 +7,32 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-
+import Games from './Games.js'
+import {games, consoles} from './Store'; 
 class App extends Component {
+    
+      state ={
+          games
+      };
+
+      getGamesbyConsoleCategory() {
+        return Object.entries(
+          this.state.games.reduce((games, game) => {
+            const {console} = game
+    
+            games[console] = games[console]
+              ? [...games[console], game]
+              : [game]
+    
+            return games
+          }, {})
+        )
+      }
+        
   render() {
+   const games = this.getGamesbyConsoleCategory();
     return (
+        <Fragment>
       <MuiThemeProvider>
           <AppBar position="static" color="default">
               <Toolbar>
@@ -24,6 +46,7 @@ class App extends Component {
                   textColor="primary"
                   fullWidth
               >
+                  <Tab label = "All"/>
                   <Tab label="Xbox One" />
                   <Tab label="PS 4" />
                   <Tab label="Nintendo" />
@@ -32,6 +55,8 @@ class App extends Component {
               </Tabs>
           </AppBar>
       </MuiThemeProvider>
+      <Games games={games}/>
+      </Fragment>     
     );
   }
 }
