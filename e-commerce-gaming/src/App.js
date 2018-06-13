aa187007f8e3263a7971282467f1fac81ae2efaa
Juan Diaz Sada <jdsada30@gmail.com>
2018-06-13 00:13:40 -0500

    changed Games.js to Games.jsx

--- e-commerce-gaming/src/App.js
+++ e-commerce-gaming/src/App.js
@@ -5,8 +5,8 @@ import './App.css';
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Games from './Components/Games'
-import {games, consoles} from './ListOfGames'; 
+import Games from './Components/Games.jsx'
+import {games} from './ListOfGames'; 
 
 class App extends Component {
     
