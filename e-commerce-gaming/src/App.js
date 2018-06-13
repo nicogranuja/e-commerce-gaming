493a8b0f79432a2548b057d1a915dd5a002d7f61
Juan Diaz Sada <jdsada30@gmail.com>
2018-06-12 22:18:34 -0500

    Moved hardcoded games from Store.js to ListOfGames.js

--- e-commerce-gaming/src/App.js
+++ e-commerce-gaming/src/App.js
@@ -6,7 +6,7 @@ import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Games from './Games.js'
-import {games, consoles} from './Store'; 
+import {games, consoles} from './ListOfGames'; 
 
 class App extends Component {
     
