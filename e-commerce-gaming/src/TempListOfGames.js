1596954d374d79fb90afdcd12ef6e9dc1c272669
Nicolas Alvarez Mondragon <nicogranuja@hotmail.com>
2018-06-26 21:59:47 -0500

    merged with dev and added the games list into a temp js file

---
+++ e-commerce-gaming/src/TempListOfGames.js
@@ -0,0 +1,39 @@
+export const consoles = ["xbox one", "ps4", "nintendo", "computer", "gameboy"];
+
+export const games = [{
+        id: "forza-horizon-4-xboxone",
+        title: "Forza Horizon 4",
+        description: "Britain Like You’ve Never Seen it. Discover lakes, valleys, castles, and breathtaking scenery all in spectacular native 4K and HDR on Xbox One",
+        console: "xbox one"
+    },
+    {
+        id: "far-cry-4-ps4",
+        title: "Far Cry 4",
+        description: "REVOLUTIONARY OPEN WORLD CO-OP: Far Cry 4 allows for a second player to drop in and out at any point, re-imagining the cooperative experience in the true spirit of Far Cry for the next generation.",
+        console: "PS4"
+    },
+    {
+        id: "far-cry-5-ps4",
+        title: "Far Cry 5",
+        description: "Fan the flames of resistance and fight to free Hope County from the grip of a deadly cult in the newest installment of the Far Cry series.",
+        console: "PS4"
+    },
+    {
+        id: "sim4",
+        title: "The Sims 4 ",
+        description: "Now with Pools and Ghosts: Pools and ghosts have arrived and are now included in The Sims 4 with the most recent updates. We've designed the game to be a live service so that we can continually evolve The Sims 4 experience with exciting new features and content.",
+        console: "computer"
+    },
+    {
+        id: "halo-5-guardians",
+        title: "Halo 5: Guardians",
+        description: "Intense story on a galactic scale: Play as the Master Chief and Spartan Locke across three new worlds.",
+        console: "xbox one"
+    },
+    {
+        id: "super-mario-galaxy-2",
+        title: "Super Mario Galaxy 2",
+        description: "Mario collects stars as he travels from galaxy to galaxy, sometimes upside-down, sometimes floating from place to place.",
+        console: "nintendo"
+    }
+]
