07a6b744e08785cb9c9e36fa3c299fb9da9f8491
Nicolas Alvarez Mondragon <nicogranuja@hotmail.com>
2018-08-05 17:30:48 -0500

    making sure only one of each game is added to the shopping cart

--- e-commerce-gaming/src/Reducers/ItemsReducer.js
+++ e-commerce-gaming/src/Reducers/ItemsReducer.js
@@ -1,22 +1,28 @@
 
 const initialState = { items:[] }
 
-function addToCartReducer(state = initialState, action) {
-
-
-    let newState = Object.assign({},state);
-    
-    switch(action.type) {
-        case 'ADD_ITEM':
-            return {
-                ...state,
-                items: [...state.items, action.item]
-            }
-        
-        default :
-            
-            return state;
+let isNewGameInCart = (gamesInCart, game) => {
+  for (let i = 0; i < gamesInCart.length; i++) {
+    if (gamesInCart[i].title === game.title) {
+      return false;
     }
+  }
+  return true;
+};
+
+let addToCartReducer = (state = initialState, action) => {
+  let newState = Object.assign({},state);
+  
+  switch(action.type) {
+    case 'ADD_ITEM':
+      if (isNewGameInCart(state.items, action.item)) {
+        return {
+          ...state,
+          items: [...state.items, action.item]
+        }
+      }
+  }
+  return state;
 }
 
 export default addToCartReducer;
