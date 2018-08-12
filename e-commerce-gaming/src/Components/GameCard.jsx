623bf1589811c99b263358f019842803f6985af5
Nicolas Alvarez Mondragon <nicogranuja@hotmail.com>
2018-08-11 21:25:17 -0500

    created custom snackbar component to be reused on other components removed from gameCard component

--- e-commerce-gaming/src/Components/GameCard.jsx
+++ e-commerce-gaming/src/Components/GameCard.jsx
@@ -10,36 +10,11 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import {Grid} from '@material-ui/core'
 import Typography from '@material-ui/core/Typography';
-import {Add, Info} from '@material-ui/icons';
+import {Add} from '@material-ui/icons';
 import { addItem } from '../Actions/AddItemsToCart';
-import CheckCircleIcon from '@material-ui/icons/CheckCircle';
 import Snackbar from '@material-ui/core/Snackbar';
-import SnackbarContent from '@material-ui/core/SnackbarContent';
-import classNames from 'classnames';
-import CloseIcon from '@material-ui/icons/Close';
-import green from '@material-ui/core/colors/green';
-import IconButton from '@material-ui/core/IconButton';
 import GameInfo from './GameInfo'
-
-const variantIcon = {
-  success: CheckCircleIcon
-}
-const styles1 = theme => ({
-  success: {
-    backgroundColor: green[600],
-  },
-  icon: {
-    fontSize: 20,
-  },
-  iconVariant: {
-    opacity: 0.9,
-    marginRight: theme.spacing.unit,
-  },
-  message: {
-    display: 'flex',
-    alignItems: 'center',
-  },
-});
+import MySnackbarContent from './CustomSnackbar'
   
 const styles = {
   card: {
@@ -66,68 +41,18 @@ const styles = {
     padding: 5,
     height: 50,
     maxHeight: 150
-
   },
   Price: {
     marginTop: -20,
     marginBottom: 20
-
   },
   More: {
     padding: 5,
-
   },
   Cart: {
     padding: 5,
-
   },
-
 };
-function MySnackbarContent(props) {
-  const { classes, className, message, onClose, variant, ...other } = props;
-  const Icon = variantIcon[variant];
-
-  return (
-    <SnackbarContent
-      className={classNames(classes[variant], className)}
-      aria-describedby="client-snackbar"
-      message={
-        <span id="client-snackbar" className={classes.message}>
-          <Icon className={classNames(classes.icon, classes.iconVariant)} />
-          {message}
-        </span>
-      }
-      action={[
-        <IconButton
-          key="close"
-          aria-label="Close"
-          color="inherit"
-          className={classes.close}
-          onClick={onClose}
-        >
-          <CloseIcon className={classes.icon} />
-        </IconButton>
-      ]}
-      {...other}
-    />
-  );
-}
-MySnackbarContent.propTypes = {
-  classes: PropTypes.object.isRequired,
-  className: PropTypes.string,
-  message: PropTypes.node,
-  onClose: PropTypes.func,
-  variant: PropTypes.oneOf(['success', 'warning', 'error', 'info']).isRequired,
-};
-const MySnackbarContentWrapper = withStyles(styles1)(MySnackbarContent);
-const styles2 = theme => ({
-  margin: {
-    margin: theme.spacing.unit,
-  },
-});
-
-
-
 
 class GameCard extends React.Component{
   state = {
@@ -144,8 +69,6 @@ class GameCard extends React.Component{
     return true;
   };
  
-
-
   handleClick = () => {
     let itemObj = {
       title: this.props.Title,
@@ -156,10 +79,9 @@ class GameCard extends React.Component{
 
     console.log(this.props.youtubeId)
 
-    if(this.isNewGameInCart(this.props.Title))
+    if (this.isNewGameInCart(this.props.Title)) {
       this.setState({ open: true });
-  
-
+    }
   }
   handleClose = (event, reason) => {
     if (reason === 'clickaway') {
@@ -167,15 +89,6 @@ class GameCard extends React.Component{
     }
     this.setState({ open: false });
   };
-
-  handleClick2 = () => {
-    
-    console.log(this.props.Description); 
-  
-
-  }
-
-
 
   render() {
     const classes = this.props;    
@@ -199,10 +112,6 @@ class GameCard extends React.Component{
         <CardActions>
           <Grid  container spacing='12'>
             <Grid item style={styles.More} xs='12' sm='6'>
-           {/*  <Button onClick={(e) => this.handleClick2(classes.Title,classes.price)} className={classes.classes.button} >
-                MORE
-                <Info width='auto'/>
-    </Button>*/}
               <GameInfo price={this.props.price} imgURL={this.props.imgURL} Title={this.props.Title} Description={this.props.Description} youtubeId={this.props.youtubeId} classButtonName={classes.classes.button} classImageName = {classes.classes.img}/>
             </Grid>
 
@@ -224,7 +133,7 @@ class GameCard extends React.Component{
           autoHideDuration={1000}
           onClose={this.handleClose}
         >
-          <MySnackbarContentWrapper
+          <MySnackbarContent
             onClose={this.handleClose}
             variant="success"
             message="Game was successfully added to cart!"
